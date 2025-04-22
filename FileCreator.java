fcb85d9640f414e2a625963f2bfe87e1e2c10867
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-04-22 17:50:25 -0400

    Create FileCreator.java

---
+++ FileCreator.java
@@ -0,0 +1,13 @@
+// Write your name here
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCreator {
+
+    public void createFileAndPopulate(String fileName, String data) {
+        System.out.println("--- Creating and populating " + fileName + " (Byte Stream) ---");
+        // Write your code here
+      
+    }
+}
