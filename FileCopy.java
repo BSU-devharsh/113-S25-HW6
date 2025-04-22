1742789b46422a5ca4b657ab6461b17c2c15dc02
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-04-22 17:54:22 -0400

    Create FileCopy.java

---
+++ FileCopy.java
@@ -0,0 +1,14 @@
+// Write your name here
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy {
+
+    public void copyFile(String sourceFile, String destinationFile) {
+        System.out.println("\n--- Copying " + sourceFile + " to " + destinationFile + " (Byte Stream with Loop) ---");
+        // Write your code here
+      
+    }
+}
