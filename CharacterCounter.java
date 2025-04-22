4b9cca463bae36bf5865ffe61040f3041d704b28
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-04-22 17:52:44 -0400

    Create CharacterCounter.java

---
+++ CharacterCounter.java
@@ -0,0 +1,14 @@
+// Write your name here
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CharacterCounter {
+
+    public void countCharacter(String fileName, char target) {
+        System.out.println("\n--- Counting occurrences of '" + target + "' in " + fileName + " (Character Stream with Loop) ---");
+        // Write your code here
+      
+    }
+}
